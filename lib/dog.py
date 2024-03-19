4a7f9a071d5683ca2de5974ca99105a3ee7dc6d2
wendymackennah <wendymackennah5@gmail.com>
2024-03-19 10:30:02 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,38 @@
-from models import Dog
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+from models import Base, Dog
 
-def create_table(base):
-    pass
+def create_table(Base,engine):
+    # engine = create_engine('sqlite:///dogs.db', echo=True)
+    Base.metadata.create_all(engine)
+    # return engine  
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+# Example usage
+# engine = create_engine('sqlite:///dogs.db', echo=True)
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# create_table(Base)
+
+# Now you can use other functions like save(), get_all(), etc.
+
